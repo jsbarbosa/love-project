5f91a0feeab5a45c40af2219afb7c6fcd3f94c34
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2019-08-28 22:10:49 -0500

    agreement

--- reboot/love.py
+++ reboot/love.py
@@ -4,15 +4,21 @@ import webbrowser
 from PyQt5.QtWidgets import QApplication, QMessageBox
 from time import sleep
 
+agreement = """Reconociendonos como seres humanos imperfectos,
+con más o menos defectos, decidimos asociarnos en el Team Cachetes
+a manera de simbiosis mutualista para avanzar juntos como pareja
+hasta donde el universo lo permita."""
+
+agreement = agreement.replace('\n', ' ')
+
 def ask():
-    return QMessageBox.question(None, "Will you?", "Quieres ser mi novia?",
+    return QMessageBox.question(None, "Will you?", agreement + "\n\nQuieres ser mi novia?",
                          QMessageBox.Yes | QMessageBox.No,
                          QMessageBox.No)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     while True:
         ans = ask()
         if ans == QMessageBox.Yes:
